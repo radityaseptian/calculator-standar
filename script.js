3beea7f183b527f64015f44542061a846860309b
radityaseptian <radityaseptian1551@gmail.com>
2022-11-23 11:25:50 +0700

    fix bug

--- script.js
+++ script.js
@@ -21,171 +21,169 @@ output.onclick = setValue
 // output
 let string = '0'
 // =====
-// output jumlah
-let nilai = 0
-// =====
 // string sementara
-let nilaiSementara = ''
+let nilai = ''
 // =====
 function setValue(e) {
    switch (e.target.id) {
       case '0':
          if (string !== '0') {
             string += '0'
-            nilaiSementara += '0'
+            nilai += '0'
          }
          output.textContent = string
          break
       case '1':
          if (string !== '0') {
             string += '1'
-            nilaiSementara += '1'
+            nilai += '1'
          } else {
             string = '1'
-            nilaiSementara = '1'
+            nilai = '1'
          }
          output.textContent = string
          break
       case '2':
          if (string !== '0') {
             string += '2'      
-            nilaiSementara += '2'
+            nilai += '2'
          } else {
             string = '2'
-            nilaiSementara = '2'
+            nilai = '2'
          }
          output.textContent = string
          break
       case '3':
          if (string !== '0') {
             string += '3'
-            nilaiSementara += '3'
+            nilai += '3'
          } else {
             string = '3'
-            nilaiSementara = '3'
+            nilai = '3'
          }
          output.textContent = string
          break
       case '4':
          if (string !== '0') {
             string += '4'
-            nilaiSementara += '4'
+            nilai += '4'
          } else {
             string = '4'
-            nilaiSementara = '4'
+            nilai = '4'
          }
          output.textContent = string
          break
       case '5':
          if (string !== '0') {
             string += '5'
-            nilaiSementara += '5'
+            nilai += '5'
          } else {
             string = '5'
-            nilaiSementara = '5'
+            nilai = '5'
          }
          output.textContent = string
          break
       case '6':
          if (string !== '0') {
             string += '6'
-            nilaiSementara += '6'
+            nilai += '6'
          } else {
             string = '6'
-            nilaiSementara = '6'
+            nilai = '6'
          }
          output.textContent = string
          break
       case '7':
          if (string !== '0') {
             string += '7'
-            nilaiSementara += '7'
+            nilai += '7'
          } else {
             string = '7'
-            nilaiSementara = '7'
+            nilai = '7'
          }
          output.textContent = string
          break
       case '8':
          if (string !== '0') {
             string += '8'
-            nilaiSementara += '8'
+            nilai += '8'
          } else {
             string = '8'
-            nilaiSementara = '8'
+            nilai = '8'
          }
          output.textContent = string
          break
       case '9':
          if (string !== '0') {
             string += '9'
-            nilaiSementara += '9'
+            nilai += '9'
          } else {
             string = '9'
-            nilaiSementara = '9'
+            nilai = '9'
          }
          output.textContent = string
          break
       case 'hapus':
          if (string.length <= 1) {
             string = '0'
-            nilaiSementara = ''
+            nilai = ''
          } else {
-            string = string.slice(0, -1)
-            nilaiSementara = nilaiSementara.slice(0, -1)
+            let index = nilai.length
+            if (nilai[index - 1] === ' ') {
+               string = string.slice(0,-3)
+               nilai = nilai.slice(0,-3)
+            } else {
+               string = string.slice(0, -1)
+               nilai = nilai.slice(0, -1)
+            }
          }
          output.textContent = string
          break
       case 'kali':
          if (string !== '0') {
             string += ' x '
-            nilai === 0 ? nilai = Number(nilaiSementara) : nilai *= Number(nilaiSementara)
-            nilaiSementara = ''
+            nilai += ' * '
             output.textContent = string
          }
          break
       case 'bagi':
          if (string !== '0') {
             string += ' / '
-            nilai === 0 ? nilai = Number(nilaiSementara) : nilai /= Number(nilaiSementara)
-            nilaiSementara = ''
+            nilai += ' / '
             output.textContent = string
          }
          break
       case 'clear':
-         nilaiSementara = ''
-         nilai = 0
+         nilai = ''
          string = '0'
          output.textContent = string
          break
       case 'kurang':
          if (string !== '0') {
             string += ' - '
-            nilai === 0 ? nilai = Number(nilaiSementara) : nilai -= Number(nilaiSementara)
-            nilaiSementara = ''
+            nilai += ' - '
             output.textContent = string
          }
          break
       case 'tambah':
          if (string !== '0') {
             string += ' + '
-            nilai === 0 ? nilai = Number(nilaiSementara) : nilai += Number(nilaiSementara)
-            nilaiSementara = ''
+            nilai += ' + '
             output.textContent = string
          }
          break
       case 'titik':
+         if (string !== '0') {
+            string += '.'
+            nilai += '.'
+            output.textContent = string
+         }
          break
       case 'jumlah':
-         if (nilai !== 0) {
-            // nilai *= Number(nilaiSementara)
-            // nilai /= Number(nilaiSementara)
-            // nilai -= Number(nilaiSementara)
-            // nilai += Number(nilaiSementara)
-            output.textContent = `=${nilai}`
-            nilaiSementara = ''
+         if (nilai !== '') {
+            output.textContent = `=${eval(nilai)}`
+            nilai = ''
             string = '0'
-            nilai = undefined
          }
          break
    }
