81a614c2d956cc2817311c2e8eb9e9bde83cddab
radityaseptian <radityaseptian1551@gmail.com>
2022-12-23 09:05:46 +0700

    validasi floating

--- script.js
+++ script.js
@@ -25,166 +25,168 @@ let string = '0'
 let nilai = ''
 // =====
 function setValue(e) {
-   switch (e.target.id) {
-      case '0':
-         if (string !== '0') {
-            string += '0'
-            nilai += '0'
-         }
-         output.textContent = string
-         break
-      case '1':
-         if (string !== '0') {
-            string += '1'
-            nilai += '1'
-         } else {
-            string = '1'
-            nilai = '1'
-         }
-         output.textContent = string
-         break
-      case '2':
-         if (string !== '0') {
-            string += '2'      
-            nilai += '2'
-         } else {
-            string = '2'
-            nilai = '2'
-         }
-         output.textContent = string
-         break
-      case '3':
-         if (string !== '0') {
-            string += '3'
-            nilai += '3'
-         } else {
-            string = '3'
-            nilai = '3'
-         }
-         output.textContent = string
-         break
-      case '4':
-         if (string !== '0') {
-            string += '4'
-            nilai += '4'
-         } else {
-            string = '4'
-            nilai = '4'
-         }
-         output.textContent = string
-         break
-      case '5':
-         if (string !== '0') {
-            string += '5'
-            nilai += '5'
-         } else {
-            string = '5'
-            nilai = '5'
-         }
-         output.textContent = string
-         break
-      case '6':
-         if (string !== '0') {
-            string += '6'
-            nilai += '6'
-         } else {
-            string = '6'
-            nilai = '6'
-         }
-         output.textContent = string
-         break
-      case '7':
-         if (string !== '0') {
-            string += '7'
-            nilai += '7'
-         } else {
-            string = '7'
-            nilai = '7'
-         }
-         output.textContent = string
-         break
-      case '8':
-         if (string !== '0') {
-            string += '8'
-            nilai += '8'
-         } else {
-            string = '8'
-            nilai = '8'
-         }
-         output.textContent = string
-         break
-      case '9':
-         if (string !== '0') {
-            string += '9'
-            nilai += '9'
-         } else {
-            string = '9'
-            nilai = '9'
-         }
-         output.textContent = string
-         break
-      case 'hapus':
-         if (string.length <= 1) {
-            string = '0'
-            nilai = ''
-         } else {
-            let index = nilai.length
-            if (nilai[index - 1] === ' ') {
-               string = string.slice(0,-3)
-               nilai = nilai.slice(0,-3)
-            } else {
-               string = string.slice(0, -1)
-               nilai = nilai.slice(0, -1)
+    switch (e.target.id) {
+        case '0':
+            if (string !== '0') {
+                string += '0'
+                nilai += '0'
             }
-         }
-         output.textContent = string
-         break
-      case 'kali':
-         if (string !== '0') {
-            string += ' x '
-            nilai += ' * '
             output.textContent = string
-         }
-         break
-      case 'bagi':
-         if (string !== '0') {
-            string += ' / '
-            nilai += ' / '
+            break
+        case '1':
+            if (string !== '0') {
+                string += '1'
+                nilai += '1'
+            } else {
+                string = '1'
+                nilai = '1'
+            }
             output.textContent = string
-         }
-         break
-      case 'clear':
-         nilai = ''
-         string = '0'
-         output.textContent = string
-         break
-      case 'kurang':
-         if (string !== '0') {
-            string += ' - '
-            nilai += ' - '
+            break
+        case '2':
+            if (string !== '0') {
+                string += '2'
+                nilai += '2'
+            } else {
+                string = '2'
+                nilai = '2'
+            }
             output.textContent = string
-         }
-         break
-      case 'tambah':
-         if (string !== '0') {
-            string += ' + '
-            nilai += ' + '
+            break
+        case '3':
+            if (string !== '0') {
+                string += '3'
+                nilai += '3'
+            } else {
+                string = '3'
+                nilai = '3'
+            }
             output.textContent = string
-         }
-         break
-      case 'titik':
-         if (string !== '0') {
-            string += '.'
-            nilai += '.'
+            break
+        case '4':
+            if (string !== '0') {
+                string += '4'
+                nilai += '4'
+            } else {
+                string = '4'
+                nilai = '4'
+            }
             output.textContent = string
-         }
-         break
-      case 'jumlah':
-         if (nilai !== '') {
-            output.textContent = `=${eval(nilai)}`
+            break
+        case '5':
+            if (string !== '0') {
+                string += '5'
+                nilai += '5'
+            } else {
+                string = '5'
+                nilai = '5'
+            }
+            output.textContent = string
+            break
+        case '6':
+            if (string !== '0') {
+                string += '6'
+                nilai += '6'
+            } else {
+                string = '6'
+                nilai = '6'
+            }
+            output.textContent = string
+            break
+        case '7':
+            if (string !== '0') {
+                string += '7'
+                nilai += '7'
+            } else {
+                string = '7'
+                nilai = '7'
+            }
+            output.textContent = string
+            break
+        case '8':
+            if (string !== '0') {
+                string += '8'
+                nilai += '8'
+            } else {
+                string = '8'
+                nilai = '8'
+            }
+            output.textContent = string
+            break
+        case '9':
+            if (string !== '0') {
+                string += '9'
+                nilai += '9'
+            } else {
+                string = '9'
+                nilai = '9'
+            }
+            output.textContent = string
+            break
+        case 'hapus':
+            if (string.length <= 1) {
+                string = '0'
+                nilai = ''
+            } else {
+                let index = nilai.length
+                if (nilai[index - 1] === ' ') {
+                    string = string.slice(0, -3)
+                    nilai = nilai.slice(0, -3)
+                } else {
+                    string = string.slice(0, -1)
+                    nilai = nilai.slice(0, -1)
+                }
+            }
+            output.textContent = string
+            break
+        case 'kali':
+            if (string !== '0') {
+                string += ' x '
+                nilai += ' * '
+                output.textContent = string
+            }
+            break
+        case 'bagi':
+            if (string !== '0') {
+                string += ' / '
+                nilai += ' / '
+                output.textContent = string
+            }
+            break
+        case 'clear':
             nilai = ''
             string = '0'
-         }
-         break
-   }
+            output.textContent = string
+            break
+        case 'kurang':
+            if (string !== '0') {
+                string += ' - '
+                nilai += ' - '
+                output.textContent = string
+            }
+            break
+        case 'tambah':
+            if (string !== '0') {
+                string += ' + '
+                nilai += ' + '
+                output.textContent = string
+            }
+            break
+        case 'titik':
+            let index = string.length
+            if (string[index - 1] == '.') {
+            } else {
+                string += '.'
+                nilai += '.'
+            }
+            output.textContent = string
+            break
+        case 'jumlah':
+            if (nilai !== '') {
+                output.textContent = `=${eval(nilai)}`
+                nilai = ''
+                string = '0'
+            }
+            break
+    }
 }
